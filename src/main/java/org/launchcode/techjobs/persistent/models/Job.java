21546722457914cec7ea65e72e33f01fe761932d
i-swa <ievaswanson@u.boisestate.edu>
2024-06-08 17:54:13 -0500

    Updated Job class with ManyToOne annotation

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -4,14 +4,18 @@ package org.launchcode.techjobs.persistent.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
 
-    private String name;
-    private String employer;
+    @ManyToOne
+    @NotNull(message = "Please add an employer")
+    private Employer employer;
+
     private String skills;
 
 
@@ -19,27 +23,19 @@ public class Job extends AbstractEntity {
     }
 
     // Initialize the id and value fields.
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer anEmployer, String someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
     }
 
     // Getters and setters.
-    
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
