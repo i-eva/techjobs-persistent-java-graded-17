fab421abab5dfa34432994072d26459d0c9cdd50
i-swa <ievaswanson@u.boisestate.edu>
2024-06-08 17:34:54 -0500

    Task 3 - added field to Employer

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
 
@@ -11,9 +14,14 @@ public class Employer extends AbstractEntity {
     @Size(min = 3, max = 255, message = "Location must be between 3 and 255 characters")
     private String location;
 
+    @OneToMany(mappedBy = "employer")
+    @JoinColumn(name = "id")
+    private List<Job> jobs;
+
     public Employer(String location) {
         this(); // must be added for no-arg constructor to work, see below
         this.location = location;
+        this.jobs = new ArrayList<>();
     }
 
     public Employer() {
@@ -28,9 +36,12 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
     @Override
     public String toString() {
         return location;
     }
-
 }
